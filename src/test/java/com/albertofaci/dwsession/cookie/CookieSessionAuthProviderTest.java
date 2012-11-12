e3256284cf157d0c8306fcbc2eec023aa413a053
alberto <albertofaci@gmail.com>
2012-11-12 08:58:54 +0000

    fixed unit test for CookieSessionAuthProviderTest

--- src/test/java/com/albertofaci/dwsession/cookie/CookieSessionAuthProviderTest.java
+++ src/test/java/com/albertofaci/dwsession/cookie/CookieSessionAuthProviderTest.java
@@ -3,15 +3,16 @@ package com.albertofaci.dwsession.cookie;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.sun.jersey.core.spi.component.ComponentScope;
 import com.yammer.dropwizard.auth.Authenticator;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CookieSessionAuthProviderTest {
 	
 	private CookieSessionAuthProvider<TestSession> provider;
@@ -24,7 +25,7 @@ public class CookieSessionAuthProviderTest {
 		provider = new CookieSessionAuthProvider<TestSession>(mockAuthenticator);
 	}
 	
-	@Test
+	@Test(expected=IllegalArgumentException.class)
 	public void failsCreateProvicerWithNullAuthenticator() {
 		new CookieSessionAuthProvider<TestSession>(null);
 	}
