d9fa900bb10ef1ec0c9c17f7538c59a11be223a0
alberto <albertofaci@gmail.com>
2012-11-25 01:30:10 +0000

    little modification to cookie session mechanism (II)

---
+++ src/main/java/com/albertofaci/dwsession/cookie/CookieSession.java
@@ -0,0 +1,11 @@
+package com.albertofaci.dwsession.cookie;
+
+public interface CookieSession {
+
+	String getId();
+	
+	String getUsername();
+	
+	String getToken();
+	
+}
---
+++ src/main/java/com/albertofaci/dwsession/cookie/CookieSessionUtils.java
@@ -0,0 +1,34 @@
+package com.albertofaci.dwsession.cookie;
+
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import org.apache.commons.lang.StringUtils;
+
+
+public class CookieSessionUtils {
+	
+	public static ResponseBuilder attachSession(ResponseBuilder responseBuilder, CookieSession session) {
+		return responseBuilder
+				.cookie(createCookie(CookieConstants.TOKEN_COOKIE_NAME, session.getToken()))
+				.cookie(createCookie(CookieConstants.USER_COOKIE_NAME, session.getUsername()))
+				.cookie(createCookie(CookieConstants.SESSION_COOKIE_NAME, session.getId()));
+	}
+	
+	public static ResponseBuilder removeSession(ResponseBuilder responseBuilder) {
+		return responseBuilder
+				.cookie(clearCookie(CookieConstants.TOKEN_COOKIE_NAME))
+				.cookie(clearCookie(CookieConstants.USER_COOKIE_NAME))
+				.cookie(clearCookie(CookieConstants.SESSION_COOKIE_NAME));
+	}
+	
+	private static NewCookie createCookie(String name, String value){
+		return new NewCookie(new Cookie(name, value), "", 86400, false);
+	}
+	
+	private static NewCookie clearCookie(String name){
+		return new NewCookie(name, StringUtils.EMPTY);
+	}
+
+}
